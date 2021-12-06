f0fbf899509748f07c3632324891fd811974db6d
anas1503 <md.anas1503@gmail.com>
2021-12-07 00:04:51 +0530

    added navbar elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Landing from './components/Landing'
+import Landing from './components/Landing page/Landing'
 import About from './components/About'
 
 const App = () => {
---
+++ src/components/Landing page/Header.js
@@ -0,0 +1,53 @@
+import React from 'react' 
+import logo from '../../assets/logo.png'
+import './landing.css' 
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src={logo} className="logo" height="50px" alt="Website logo" />
+            <div className="elements">
+                <h4>About</h4>   
+                <h4>Services</h4>   
+                <h4>Campus Ambassader</h4>   
+                <h4>Material</h4>   
+                <h4>Internships</h4>   
+                <h4>Hiring</h4>   
+                <h4>Mentors</h4>   
+                <h4>Industrial Personalities</h4>   
+                {/* <ul class="navbar-nav m-auto">
+        <li class="nav-item active mx-5">
+          <a  class="nav-link" href="#home" >About</a>
+        </li>
+        <li class="nav-item mx-5">
+          <a  class="nav-link" href="#about" >Services</a>
+        </li>
+        <li class="nav-item mx-5">
+          <a  class="nav-link" href="#skill" >Campus Ambassader</a>
+        </li>
+        <li class="nav-item mx-5">
+          <a  class="nav-link" href="#contact" >Material</a>
+        </li>
+        <li class="nav-item mx-5">
+          <a  class="nav-link" href="#contact" >Internships</a>
+        </li>
+        <li class="nav-item mx-5">
+          <a  class="nav-link" href="#contact" >Hiring</a>
+        </li>
+        <li class="nav-item mx-5">
+          <a  class="nav-link" href="#contact" >Mentors</a>
+        </li>
+        <li class="nav-item mx-5">
+          <a  class="nav-link" href="#contact" >Industrial Personalities</a>
+        </li>
+
+
+      </ul> */}
+  
+            </div>
+    
+        </div>  
+    )
+}
+
+export default Header
